a4021cdfe551c541507c02546cd5110020886d60
blessing267 <blessingomokaro16@gmail.com>
2025-12-02 12:06:33 +0000

    update

--- lily/settings.py
+++ lily/settings.py
@@ -83,7 +83,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASE_URL = config("DATABASE_URL")
 
 DATABASES = {
-    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+}
+
+DATABASES['default']['OPTIONS'] = {
+    'sslmode': 'require',  # Neon needs this
 }
 
 
